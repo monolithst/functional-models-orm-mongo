60a771f0e25731ceda31cd462269ead9c69461ed
Mike Cornwell <michael.a.cornwell@gmail.com>
2025-01-25 11:42:49 -0500

    Fixes.

--- src/lib.ts
+++ src/lib.ts
@@ -1,16 +1,11 @@
 import { DataDescription, ModelType } from 'functional-models'
 import {
-  AllowableEqualitySymbols,
   DatesAfterStatement,
   DatesBeforeStatement,
   EqualitySymbol,
-  OrmQuery,
-  OrmQueryStatement,
-  ORMType,
-  PropertyOptions,
-  PropertyStatement,
-  SortStatement,
-  TakeStatement,
+  PropertyStatement, QueryTokens,
+  SearchQuery,
+  threeitize,
 } from 'functional-models-orm'
 import merge from 'lodash/merge'
 
@@ -53,6 +48,7 @@ const buildDateQueries = (
     })
   }, before)
 }
+/*
 
 const buildSearchQuery = (ormQuery: OrmQuery) => {
   if (!ormQuery.chain) {
@@ -120,6 +116,8 @@ const buildSearchQuery = (ormQuery: OrmQuery) => {
   return merge(propertyQuery, dateEntries)
 }
 
+ */
+
 const buildMongoFindValue = (partial: PropertyStatement) => {
   const value = partial.value
   // Is this a javascript date??
@@ -153,226 +151,7 @@ const buildMongoFindValue = (partial: PropertyStatement) => {
   return { [partial.name]: value }
 }
 
-/*
-Situations
-
-[a=true]
-[a=true and b=true]
-[a=true or b=true]
-[a=[b=true or c=true] and d=[e=true and f=true]]
-
-And statement always first.
-
-Rules:
-Overall object is an array.
-
-Arrays Must:
-Be All statements (Ands)
-Be Statements separated by and/or
-Cannot end with and/or
-Cannot have two and/or in a row.
-
-Each spot can be...
-An array
-A statement
-And/OR
-
-
-
-[
-  [s1 'and' s2], 'and', [[s3 and s4], 'or', [s3 and s5], 'or' [s4 'or' s5]]
-]
-
-
- */
-
-type S = OrmQueryStatement
-type E = 'AND' | 'OR'
-type Tokens = Tokens[] | S | E
-
-type OverallQuery = Tokens[]
-
-const fu = (o: OverallQuery) => {}
-
-const property = (
-  name: string,
-  value: any,
-  {
-    caseSensitive = false,
-    startsWith = false,
-    endsWith = false,
-    type = ORMType.string,
-    equalitySymbol = EqualitySymbol.eq,
-  }: PropertyOptions = {}
-) => {
-  if (!AllowableEqualitySymbols.includes(equalitySymbol)) {
-    throw new Error(`${equalitySymbol} is not a valid symbol`)
-  }
-  if (equalitySymbol !== EqualitySymbol.eq && type === ORMType.string) {
-    throw new Error(`Cannot use a non = symbol for a string type`)
-  }
-  if (!type) {
-    type = ORMType.string
-  }
-
-  const propertyEntry: PropertyStatement = {
-    type: 'property',
-    name,
-    value,
-    valueType: type,
-    options: {
-      caseSensitive,
-      startsWith,
-      endsWith,
-      equalitySymbol,
-    },
-  }
-  return propertyEntry
-}
-
-/*
-
-[
-  [s1 'and' s2], 'and', [[s3 and s4], 'or', [s3 and s5], 'or' [s4 'or' s5]]
-]
-
- */
-fu([
-  [property('s1', 'abc'), 'AND', property('s2', 'cbd')],
-  'AND',
-  [
-    [property('s3', '123'), 'AND', property('s4', 'abc')],
-    'OR',
-    [property('s3', '123'), 'AND', property('s5', '098')],
-    'OR',
-    [
-      property('s4', 'abc'),
-      'AND',
-      property('s5', '098'),
-      'AND',
-      property('s6', '1111'),
-    ],
-  ],
-])
-
-/*
-// Builder Approach - Creates statement query
-builderV2()
-  .property('something', 'else')
-  .or()
-  .complex(x => x 
-    .property('another', 'value')
-    .and()
-    .complex(y => y
-      .property('another2', 'value2')
-      .and()
-      .property('another3', 'value3')
-    )
-  )
-  .compile()
-*/
-
-/*
-// Just writing it out approach - Writing a statement query
-[
-  property('something', 'else'),
-  'OR',
-  [
-    property('another', 'value'),
-    'AND',
-    [
-      property('another2', 'value2'),
-      'AND',
-      property('another3', 'value3'),
-    ]
-  ]
-]
-*/
-
-const link = (data: AQuery) => {
-  return {
-    and: () => {
-      return builderV2({...data, query: data.query.concat('AND')})
-    },
-    or: () => {
-      return builderV2({...data, query: data.query.concat('OR')})
-    },
-    compile: () => {
-      return data
-      //return v2({query: data})
-    }
-  }
-}
-
-type BuilderLink = {
-  and: () => BuilderV2,
-  or: () => BuilderV2,
-  compile: () => AQuery // Final query
-}
-
-type BuilderV2 = {
-  complex: (subBuilderFunc: SubBuilderFunction) => BuilderLink,
-}
-
-type SubBuilderFunction = (builder: BuilderV2|AQuery) => AQuery 
-
-
-const builderV2 = (data: AQuery|undefined=undefined) => {
-  data = data || { query: []}
-
-  const myProperty = (...args: any[]) => {
-    // @ts-ignore
-    const p = property(...args)
-    return link({...data, query: data.query.concat(p)})
-  }
-
-  const complex = (subBuilderFunc: SubBuilderFunction) => {
-    const subBuilder = builderV2()
-    const result = subBuilderFunc(subBuilder)
-    console.log("subbuilder result")
-    console.log(result)
-    // @ts-ignore
-    if (result.compile) {
-      // @ts-ignore
-      return link({...data, query: data.query.concat([result.compile().query])})
-    }
-      // @ts-ignore
-    return link({...data, query: data.query.concat([result.query])})
-  }
-
-  const take = (num: number) => {
-    const parsed = parseInt(num as unknown as string, 10)
-    if (Number.isNaN(parsed)) {
-      throw new Error(`${num} must be an integer.`)
-    }
-    return link({...data, take: {
-      type: 'take',
-      value: parsed,
-    }})
-  }
-
-  const sort = (key: string, isAscending = true) => {
-    if (typeof isAscending !== 'boolean') {
-      throw new Error('Must be a boolean type')
-    }
-    const sortStatement: SortStatement = {
-      type: 'sort',
-      key,
-      order: isAscending,
-    }
-    return link({...data, sort: sortStatement})
-  }
-
-  return {
-    complex,
-    property: myProperty,
-    take,
-    sort,
-  }
-}
-
-type Either = "$and"|"$or"
-const processMongoArray = (o: Tokens[]): { 
+const processMongoArray = (o: QueryTokens[]): {
   "$and"?: any,
   "$or"?: any,
 } => {
@@ -420,13 +199,7 @@ const processMongoArray = (o: Tokens[]): {
  */
 }
 
-const doProperty = (p: PropertyStatement) => {
-  return {
-    [p.name]: p.value,
-  }
-}
-
-const handleMongoQuery = (o: Tokens) => {
+const handleMongoQuery = (o: QueryTokens) => {
   if (Array.isArray(o)) {
     return processMongoArray(o)
   }
@@ -434,32 +207,32 @@ const handleMongoQuery = (o: Tokens) => {
     throw new Error(``)
   }
   if (o.type === 'property') {
-    return doProperty(o)
+    return buildMongoFindValue(o)
+  }
+  if (o.type === 'datesBefore') {
+      return {
+        [o.key]: {
+          [`$lt${o.options.equalToAndBefore ? 'e' : ''}`]:
+            o.date instanceof Date
+              ? o.date.toISOString()
+              : o.date,
+        },
+      }
+  }
+  if (o.type === 'datesAfter') {
+      return {
+        [o.key]: {
+          [`$gt${o.options.equalToAndAfter ? 'e' : ''}`]:
+            o.date instanceof Date
+              ? o.date.toISOString()
+              : o.date,
+        },
+      }
   }
   throw new Error('Unhandled currently')
 }
 
-type AQuery = {
-  take?: TakeStatement
-  sort?: SortStatement
-  query: OverallQuery
-}
-
-const threeitize = <T>(data: T[]): T[][] => {
-  if (data.length === 0 || data.length === 1) {
-    return []
-  }
-  if (data.length < 3) {
-    throw new Error('Must include at least 3 items')
-  }
-  const three = data.slice(0, 3)
-  const rest = data.slice(2)
-  const moreThrees = threeitize(rest)
-  return [three, ...moreThrees]
-}
-
-
-const v2 = (o: AQuery) => {
+const v2 = (o: SearchQuery) => {
   return [
     {
       $match: handleMongoQuery(o.query),
@@ -468,13 +241,8 @@ const v2 = (o: AQuery) => {
 }
 
 export {
-  AQuery,
   buildDateQueries,
   buildMongoFindValue,
-  buildSearchQuery,
   getCollectionNameForModel,
-  OverallQuery,
-  property,
   v2,
-  builderV2,
 }
